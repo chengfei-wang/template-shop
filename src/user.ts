6fc58f9540bb6496ee046348eb846871775ccbea
yiyou <yiyou@mogu.com>
2022-03-23 21:23:49 +0800

    modify user def

--- src/user.ts
+++ src/user.ts
@@ -1,28 +1,18 @@
 import {request} from "./requests"
 
 export class User {
-    constructor(uid: string, name: string, email: string, token: string) {
+    constructor(uid: string, name: string, email: string) {
         this.uid = uid
         this.name = name
         this.email = email
-        this.token = token
     }
 
     uid: string
     name: string
     email: string
-    token: string
 }
 
 let storage = window.localStorage
-
-export function getUser(): User {
-    let uid = storage.getItem("uid")
-    let name = storage.getItem("name")
-    let email = storage.getItem("email")
-    let token = storage.getItem("token")
-    return new User(uid!, name!, email!, token!);
-}
 
 export function logout() {
     storage.clear()
