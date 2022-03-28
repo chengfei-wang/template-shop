d2b7a1a2efb8151eb948ba4f75990b2fe545a069
yiyou <yiyou@mogu.com>
2022-03-28 18:59:04 +0800

    减少DIV层级

--- src/render.ts
+++ src/render.ts
@@ -1,8 +1,10 @@
 import { NodeProp } from "./widget";
 
 export function render_node_prop(html: string, prop: NodeProp): string {
-    let element = document.createElement("div")
+    let element: HTMLElement = document.createElement("div")
     element.innerHTML = html
+
+    element = <HTMLElement>element.childNodes[0]
 
     if (prop?.clazz != null) {
         prop.clazz.forEach(clazz => {
